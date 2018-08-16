e7af73378bb381d21770d8b2bf91308c35aa178d
pr08 <pr08@figd-1711708.figdtk.de>
2018-08-16 15:13:58 +0200

    1st Commmit 16.08.

---
+++ assets/js/weather.js
@@ -0,0 +1,113 @@
+$(document).on("pagecreate", "#page_home",function(){
+    var $content = $('#content_weather');
+    var $description = $('#content_description');
+    var $place = $('#content_place');
+    var $pressure = $('#content_pressure');
+    var $selectCity = $('#select_city'); 
+    var $selectTmp = $('.selectBtnTmp');
+    var $selectCountry = $('.selectBtnCountry');
+    var $forecastTmp = $('#forecastTmp'); 
+    var $forecastIcon = $('#forecastIcon'); 
+    var $forecastHour = $('#forecastHour'); 
+    $content.html("It works");
+    
+    var opts={
+        q:'Tokyo',
+        units:'metric',
+        lang:'de',
+        APPID:'e56c5866972cebc60f3d29e6b258800c'
+    }
+    init();
+    function init(){
+        var url=getApiUrl(opts);
+        var forecastUrl = getForecastUrl(opts);
+        $.getJSON(url, viewData);
+        $.getJSON(forecastUrl, viewForecastData);
+        $selectCity.change(changeCity);
+        $selectTmp.click(changeTmp);      
+        $selectCountry.click(changeLang);              
+    }  
+    
+    function changeCity(){
+        $place.html("");
+        $description.html("");
+        $pressure.html("");
+        opts.q = $(this).val();          
+        $.getJSON(getApiUrl(opts), viewData);
+        $.getJSON(getForecastUrl(opts), viewForecastData);
+    }
+    function changeTmp(){
+        $place.html("");
+        $description.html("");
+        $pressure.html("");
+        opts.units = $(this).attr('data-tmp');          
+        $.getJSON(getApiUrl(opts), viewData);
+    }
+    function changeLang(){
+        $place.html("");
+        $description.html("");
+        $pressure.html("");
+        opts.lang = $(this).attr('data-lang');   
+        $.getJSON(getApiUrl(opts), viewData);          
+    }
+    //bei einer klaren Datenlieferung per JSON kann man nehmen
+        
+    function viewData(data){          
+        var icon = data.weather[0].icon;
+        var iconUrl = 'https://api.openweathermap.org/img/w/'+ icon + '.png';
+        var picUrl = 'assets/images/'+data.name+'.jpg';
+        $content.html(null);
+        $('<h2>').text(data.name).appendTo($place);
+        $('<img>').attr('src', picUrl).css({
+            width:'120px',
+            height:'150px'
+        }).appendTo($place);          
+        $('<h3>').text('Temperatur: ' + parseInt(data.main.temp) + '°').appendTo($content);
+        $('<h4>').text('Höchsttemperatur: ' + data.main.temp_max + '°').appendTo($content);
+        $('<h4>').text('Tiefsttemperatur: ' + data.main.temp_min + '°').appendTo($content);        
+        $('<img>').attr('src', iconUrl).appendTo($content); 
+        $('<h3>').text('Nähere Information: \n').appendTo($description);
+        $('<h4>').text(data.weather[0].description).css({
+            paddingRight:'5%'
+        }).appendTo($description);
+        $('<h4>').text('\nWind: ' + data.wind.speed + ' km/h').appendTo($description);        
+        $('<h3>').text('Luftdruck: ').appendTo($pressure);
+        $('<h4>').text(data.main.pressure + ' Pascal').appendTo($pressure);
+    }
+    function getForecastUrl(args){
+        var url='https://api.openweathermap.org/data/2.5/forecast?';          
+        $.each(args, function(key,value){
+            url += key + '=' + value + "&"; 
+        });        
+        return url;        
+    }
+    function viewForecastData(data){
+        $forecastHour.html("");
+        $forecastTmp.html("");
+        $forecastIcon.html("");
+        for (var i = 0, max = 5; i < max; i++) {
+            var chars =["a","b","c","d","e"];
+            var tmp = data.list[i].main.temp;
+            var forecastIcon = data.list[i].weather[0].icon;
+            var iconUrl = 'https://api.openweathermap.org/img/w/'+ forecastIcon + '.png';
+            var block1 = $('<div class="ui-block-'+chars[i]+'">');              
+            block1.text("~ "+(i+1)*3+" h:").appendTo($forecastHour);
+            var block2 = $('<div class="ui-block-'+chars[i]+'">');              
+            block2.html("\n<strong>" + tmp + " °</strong>\n").appendTo($forecastTmp);
+            var block3 = $('<div class="ui-block-'+chars[i]+'">');              
+            $('<img>').attr('src', iconUrl).appendTo(block3); 
+            block3.appendTo($forecastIcon);
+            //$('<div class="ui-block-a"><div class="ui-bar ui-bar-a" style="height:60px">').img(forecastIcon).appendTo($forecastIcon);
+        }
+    }
+    function getApiUrl(args){
+        var url='https://api.openweathermap.org/data/2.5/weather?';          
+        $.each(args, function(key,value){
+            url += key + '=' + value + "&"; 
+        });        
+        return url;
+    };
+    
+    
+});
+
---
+++ index.php
@@ -0,0 +1,81 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>jQuery Mobile page</title>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="assets/css/themes/mycolors.css" />
+        <link rel="stylesheet" href="assets/css/themes/jquery.mobile.icons.min.css" />
+        <link href="assets/css/styles.css" rel="stylesheet" type="text/css"/>
+        <!--<link rel="stylesheet" href="http://code.jquery.com/mobile/1.4.5/jquery.mobile.structure-1.4.5.min.css" /> 
+        <script src="http://code.jquery.com/jquery-1.11.1.min.js"></script> 
+        <script src="http://code.jquery.com/mobile/1.4.5/jquery.mobile-1.4.5.min.js"></script> -->
+        <link rel="stylesheet" href="assets/css/jquery.mobile.structure-1.4.5.min.css" /> 
+        <script src="assets/js/jquery-1.11.1.min.js"></script> 
+        <script src="assets/js/jquery.mobile-1.4.5.min.js"></script>
+        <script src="assets/js/weather.js" type="text/javascript"></script>
+
+    </head>
+    <body>
+
+        <div data-role="page" id="page_home">
+            <div data-role="header" data-position="fixed">
+                <h1>Das Wetter</h1>
+            </div>
+            <div data-role="content"> 
+                <div data-role="panel" id="sideNav" data-display="overlay"><!-- oder auch push oder reveal-->
+                    <div data-role="controlgroup" id="select_tmp" data-type="horizontal">                
+                        <a href="#" class="ui-btn ui-corner-all selectBtnTmp" data-tmp="fahrenheit" data-rel="close">Fahrenheit</a>
+                        <a href="#" class="ui-btn ui-corner-all selectBtnTmp" data-tmp="metric" data-rel="close">Celsius</a>                
+                    </div>
+                    
+                    <div data-role="controlgroup" id="select_country" data-type="horizontal">                
+                        <a href="#" class="ui-btn ui-corner-all selectBtnCountry" data-rel="close" data-lang="de"><img src="assets/images/german.png" width="40" height="40" alt="german flag"/></a>
+                        <a href="#" class="ui-btn ui-corner-all selectBtnCountry" data-rel="close" data-lang="en"><img src="assets/images/english.png" width="40" height="40" alt="english flag"/></a>
+                        <a href="#" class="ui-btn ui-corner-all selectBtnCountry" data-rel="close" data-lang="fr"><img src="assets/images/french.png" width="40" height="40" alt="french flag"/></a>                                        
+                    </div>
+                    <a href="#" class="ui-btn" data-rel="close">CLOSE</a>                
+                </div>                
+                <a href="#sideNav" class="ui-btn ui-icon-bars ui-btn-icon-left ui-icon-gear">EINSTELLUNGEN</a>
+                <div class="ui-field-contain">    
+                    <select data-native-menu="false" id="select_city" data-iconpos="left">
+                        <option value="">Bitte Stadt wählen</option>        
+                        <option value="Bangkok">Bangkok</option>
+                        <option value="Berlin, de">Berlin</option>
+                        <option value="London">London</option>
+                        <option value="New York">New York</option>
+                        <option value="Paris">Paris</option>
+                        <option value="Tokyo">Tokyo</option>
+                        <option value="Rio">Rio</option>
+                        <option value="Lima">Lima</option>
+                        <option value="Honolulu">Honolulu</option>                        
+                    </select>
+                </div>
+                <div class="ui-grid-a"><!--grid-a ist zweispaltig-->
+                    <div class="ui-block-a" id="content_place">
+
+                    </div>
+                    <div class="ui-block-b">
+                        <div id="content_weather"></div>                            
+                    </div>
+                    <hr>
+                    <div class="ui-block-a">
+                        <div id="content_description"></div>                            
+                    </div>
+                    <div class="ui-block-b">
+                        <div id="content_pressure"></div>
+                    </div>
+                </div>
+                <hr>
+                <h3>Vorschau</h3>
+                <div class="ui-grid-d" id="forecastHour"></div>
+                <div class="ui-grid-d" id="forecastTmp"></div>
+                <div class="ui-grid-d" id="forecastIcon"></div>
+                <div data-role="footer" data-position="fixed">
+                    <p>&copy; Stefan Landvogt 2018</p>
+                </div>
+
+            </div>
+
+    </body>
+</html>
